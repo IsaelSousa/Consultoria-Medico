8fed7cb5d1fe640f279cf6382614bcdb7cde94b3
IsaelSousa <toxicrootbeta@gmail.com>
2022-11-26 02:22:55 -0300

    first commit

---
+++ Consultoria Médico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Consultoria_Médico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
